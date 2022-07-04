cc2c0c9ea9af71722dec3738a77d13ddf1a3ad66
INA <codechemi@gmail.com>
2022-07-04 14:00:24 +0900

    Refactor : Edit Comments.

--- WPF-MVVM/View/MainWindow.xaml.cs
+++ WPF-MVVM/View/MainWindow.xaml.cs
@@ -12,17 +12,21 @@ namespace WPF_MVVM
         public MainWindow()
         {
             InitializeComponent();
+
             AppViewModel AppView = new AppViewModel();
+            
+            // View와 ViewModel 바인딩
             DataContext = AppView;
 
             // XAML 파일이 손상되어 바인딩 정보가 날아가는 것을 방지하기 위해 코드 바인딩 사용
-
+            // Target : View, Source : ViewModel
             Binding bind = new Binding();
             bind.Source = DataContext;
-            // AppNotify.OnPropertyChanged Message를 Path로 지정해야한다. 
+            // Source : ViewModel.Property
             bind.Path = new PropertyPath("TitleContent");
             bind.UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged;
             bind.Mode = BindingMode.TwoWay;
+            // Target : View.Control
             LblTitle.SetBinding(ContentProperty, bind);
 
             BtnActionEvent.Command = AppView.AppCmd;
